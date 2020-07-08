6e13db9603d69f7d75e266847ba3c4b676879475
Seongtaek Lim <sl2838@cornell.edu>
2020-07-08 11:49:26 -0700

    add serve; add profile photo

--- serve.sh
+++ serve.sh
@@ -1,2 +1,3 @@
-bundle exec jekyll serve \
-  --config _config.yml,_config-dev.yml
+#bundle exec jekyll serve \
+#  --config _config.yml,_config-dev.yml
+cd ${PROJECT_ROOT}/_site/ && python -m SimpleHTTPServer 4000 && cd ${PROJECT_ROOT}
